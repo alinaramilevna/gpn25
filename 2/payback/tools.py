95e16e1068007522bf00624fe5bf91ccd290167f
alinara <alina.kush2009@gmail.com>
2025-03-10 18:45:09 +0500

    bugfix

--- 2/payback/tools.py
+++ 2/payback/tools.py
@@ -9,3 +9,23 @@ def get_daily_by_dupuis(t: int, qi: float = 10_000, di: float = 0.1, days: int =
     # формула Дюпюи для линейного падения
     qt = qi * (1 - di_daily * t)
     return max(qt, 0)
+
+
+def get_sum_by_year_by_dupuis(t: int, qi: float = 10_000, di: float = 0.1) -> float:
+    s = 0
+    curr_debit = qi * 365
+    for year in range(1, t + 1):
+        qt = curr_debit * (1 - di)
+        s += qt
+        curr_debit = qt
+
+    return s
+
+
+def get_curr_debit_by_dupuis(t: int, qi: float = 10_000, di: float = 0.1) -> float:
+    curr_debit = qi * 365
+    for year in range(1, t + 1):
+        qt = curr_debit * (1 - di)
+        curr_debit = qt
+
+    return curr_debit
--- 2/payback/with_dupuis.py
+++ 2/payback/with_dupuis.py
@@ -1,27 +1,29 @@
+'''
+В файле приведен расчет с учетом падения дебита
+'''
 from math import ceil
 
 from consts import electric_submersible_pumps_energy, e_price, esp_frequency_control_energy, \
     esp_frequency_control_price, gaslift_energy, gaslift_price, discounting
-from tools import get_daily_by_dupuis
+from tools import get_daily_by_dupuis, get_sum_by_year_by_dupuis, get_curr_debit_by_dupuis
 
 
-def get_barrels_per_year(year: int = 1) -> float:
-    # Учитываем падение дебита для всего года, начиная с первого года
-    return sum([get_daily_by_dupuis(t, di=0.1 * year) for t in range(1, 366)])
+def get_barrels_per_year() -> float:
+    # возьмем 365 дней в году
+    return sum([get_daily_by_dupuis(t) for t in range(1, 366)])
 
 
-def get_barrels_per_period(t: int, year: int = 1) -> float:
-    # Учитываем падение дебита для каждого периода (по дням), с накоплением за годы
-    return sum([get_daily_by_dupuis(i, di=0.1 * year, days=t) for i in range(1, t + 1)])
+def get_barrels_per_period(t) -> float:
+    return sum([get_daily_by_dupuis(i) for i in range(1, t + 1)])
 
 
 # ------------------------------------------------------
 
-def get_electric_submersible_pumps_cost(years: int = 1) -> int:
+
+def get_electric_submersible_pumps_cost(year: int = 1) -> int:
     # 1. Переводим Дж в кВт·ч (1 кВт·ч = 3 600 000 Дж)
-    # 2. Умножаем на стоимость электроэнергии с учетом падения дебита за все годы
-    return ceil(
-        (electric_submersible_pumps_energy / 3_600_000) * e_price * get_barrels_per_period(int(365 * years), years))
+    # 2. Умножаем на стоимость электроэнергии
+    return ceil((electric_submersible_pumps_energy / 3_600_000) * e_price * get_sum_by_year_by_dupuis(year))
 
 
 def get_electric_submersible_pumps_PBP() -> float:
@@ -31,8 +33,8 @@ def get_electric_submersible_pumps_PBP() -> float:
 
 # ------------------------------------------------------
 
-def get_esp_frequency_control_cost(years: int = 1) -> int:
-    return ceil((esp_frequency_control_energy / 3_600_000) * e_price * get_barrels_per_period(365 * years, years))
+def get_esp_frequency_control_cost(year: int = 1) -> int:
+    return ceil((esp_frequency_control_energy / 3_600_000) * e_price * get_sum_by_year_by_dupuis(year))
 
 
 def get_esp_frequency_control_PBP() -> float:
@@ -40,8 +42,8 @@ def get_esp_frequency_control_PBP() -> float:
     return esp_frequency_control_price / savings
 
 
-def get_gaslift_cost(years: int = 1) -> int:
-    return ceil((gaslift_energy / 3_600_000) * e_price * get_barrels_per_period(365 * years, years))
+def get_gaslift_cost(year: int = 1) -> int:
+    return ceil((gaslift_energy / 3_600_000) * e_price * get_sum_by_year_by_dupuis(year))
 
 
 def get_gaslift_PBP() -> float:
@@ -50,6 +52,7 @@ def get_gaslift_PBP() -> float:
 
 
 # ------------------------------------------------------
+
 
 def get_gaslift_DPBP() -> float:
     discounting_savings = (get_electric_submersible_pumps_cost() - get_gaslift_cost()) / (1 + discounting)
@@ -71,15 +74,21 @@ def get_esp_frequency_control_DPBP() -> float:
 def get_gaslift_NPV(t: int = 1) -> float:
     npv = 0
     for year in range(1, t + 1):
-        npv += (get_electric_submersible_pumps_cost(year) - get_gaslift_cost(year)) / (1 + discounting) ** year
+        electric_submersible_pumps_cost = ceil(
+            (electric_submersible_pumps_energy / 3_600_000) * e_price * get_curr_debit_by_dupuis(year))
+        gaslift_cost = ceil((gaslift_energy / 3_600_000) * e_price * get_curr_debit_by_dupuis(year))
+        npv += (electric_submersible_pumps_cost - gaslift_cost) / (1 + discounting) ** year
     return npv
 
 
 def get_esp_frequency_control_NPV(t: int = 1) -> float:
     npv = 0
     for year in range(1, t + 1):
-        npv += (get_electric_submersible_pumps_cost(year) - get_esp_frequency_control_cost(year)) / (
-                1 + discounting) ** year
+        electric_submersible_pumps_cost = ceil(
+            (electric_submersible_pumps_energy / 3_600_000) * e_price * get_curr_debit_by_dupuis(year))
+        esp_frequency_control_cost = ceil(
+            (esp_frequency_control_energy / 3_600_000) * e_price * get_curr_debit_by_dupuis(year))
+        npv += (electric_submersible_pumps_cost - esp_frequency_control_cost) / (1 + discounting) ** year
     return npv
 
 
@@ -101,13 +110,10 @@ def get_esp_frequency_control_PI(t: int = 1) -> float:
 print('С УЧЕТОМ ПАДЕНИЯ ДЕБИТА')
 print('PBP ЭЦН с частотным регулированием:', get_esp_frequency_control_PBP())
 print('DPBP ЭЦН с частотным регулированием:', get_esp_frequency_control_DPBP())
-print('NPV ЭЦН с частотным регулированием:', get_esp_frequency_control_NPV(16))
-print('PI ЭЦН с частотным регулированием:', get_esp_frequency_control_PI(16))
+print('NPV ЭЦН с частотным регулированием:', get_esp_frequency_control_NPV())
+print('PI ЭЦН с частотным регулированием:', get_esp_frequency_control_PI(30))
 print('----------------------------------------------------------------------------')
 print('PBP Газлифтной системы:', get_gaslift_PBP())
 print('DPBP Газлифтной системы:', get_gaslift_DPBP())
-print('NPV Газлифтной системы:', get_gaslift_NPV(16))
-print('PI Газлифтной системы:', get_gaslift_PI(16))
-print('Дебит ДО/ПОСЛЕ перерасчета по формуле ДЮПЮИ')
-print(get_barrels_per_period(16 * 365, 16))  # Падение дебита за 16 лет
-print(10_000 * 365 * 16)  # Проверка для сравнения
+print('NPV ЭЦН Газлифтной системы:', get_gaslift_NPV())
+print('PI ЭЦН Газлифтной системы:', get_gaslift_PI(30))
